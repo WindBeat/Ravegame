35f3f06a32e7c77ff08b8a34b930f7d040332e9c
Jordan Walker <jordanwalker465@gmail.com>
2014-06-16 18:01:29 -0700

    Create a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ravegame::Application.config.secret_token = '2f27afc922ff089bf2919aa446d4698f361222e82e02c356a0c76482768b4f405f73c6cdf7cc29e8e3dc2e2b8616eebc8addeb3e8e71b13d5046e6407484f597'
